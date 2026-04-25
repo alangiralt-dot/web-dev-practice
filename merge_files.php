211441bf13193016a383bc1d2f49a66fbce32e00
Alan GIRALT <alangiralt@telefonica.net>
2026-04-25 09:55:47 +0200

    refactor(merge_files): use GLOB_BRACE for multiple extensions

--- merge_files.php
+++ merge_files.php
@@ -2,18 +2,14 @@
 declare(strict_types=1);
 
 // 1. Specify the absolute path to your exercise folder
-$fullPath = 'C:/xampp/htdocs/alan/tasca_s1_05/nivell_1/exercici_2/';
+$fullPath = 'C:/xampp/htdocs/alan/tasca_s1_06/nivell_1/exercici_1/';
 
 if (!is_dir($fullPath) || !is_writable($fullPath)) {
     exit("Error: The directory $fullPath is not ready for writing.");
 }
 
 // 2. Specify the files within that folder
-$filePaths = glob($fullPath . "*.php");
-$umlDiagram = $fullPath . 'diagram.puml';
-if (file_exists($umlDiagram) && is_writable($umlDiagram)) {
-    array_push($filePaths, $umlDiagram);
-}
+$filePaths = glob($fullPath . "*.{php,html,css,puml}", GLOB_BRACE);
 
 // 3. Specify where to save the result
 $outputFile = 'C:/xampp/htdocs/alan/ai_info.txt';
